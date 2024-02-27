c705b25c510fdc516e726a41c9043721ab33ebab
Roland <143491047+AthenaRoland@users.noreply.github.com>
2024-02-27 14:49:50 +0800

    Delete Watching-for-windows directory

--- Watching-for-windows/methods/userdataschange.py
+++
@@ -1,5 +0,0 @@
-class UserDatasChange():
-    def __init__(self,filepath, value):
-        with open(f"{filepath}", "w", encoding="utf-8") as file:
-            file.write(value)
-            file.close()
